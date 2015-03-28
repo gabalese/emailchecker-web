ebfad0b750b1133dbd51dde901d747c40b344d90
Gabriele Alese <gabriele@alese.it>
2015-03-28 14:56:00 +0000

    Check endpoint

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,17 +1,16 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 import it.alese.emailchecker.EmailChecker
 
 object Application extends Controller {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
-  }
-
   def check(email: String) = Action {
     val check = EmailChecker.check(email)
     Ok(check.toString)
+  }
+
+  def home() = Action {
+    Ok("Hello")
   }
 }
--- build.sbt
+++ build.sbt
@@ -7,7 +7,7 @@ lazy val `emailcheckerweb` = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.1"
 
 libraryDependencies ++= Seq( jdbc , anorm , cache , ws,
-  "it.alese" %% "email-checker" % "1.0",
+  "it.alese" %% "email-checker" % "0.1",
   "dnsjava" % "dnsjava" % "2.1.6"
 )
 
