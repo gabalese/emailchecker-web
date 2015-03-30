5f244188f1d4999d2a48c6ba06c2618d897b5c63
Gabriele Alese <gabriele@alese.it>
2015-03-30 20:44:39 +0100

    Implemented JSON responses

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,16 +1,26 @@
 package controllers
 
 import play.api.mvc._
-import it.alese.emailchecker.EmailChecker
+import it.alese.emailchecker._
+import play.api.libs.json._
 
 object Application extends Controller {
 
   def check(email: String) = Action {
-    val check = EmailChecker.check(email)
-    Ok(check.toString)
+    val check: ServiceResponse = EmailChecker.check(email)
+
+    check match {
+      case RecipientOK => Ok(Json.obj("status" -> "success", "message" -> "Recipient exists"))
+      case DoesNotExist => Ok(Json.obj("status" -> "failure", "message" -> "Recipient does not exist"))
+      case ServerUnreachable => Ok(Json.obj("status" -> "failure", "message" -> "Unable to connect to mail server"))
+      case IPBlacklisted => Ok(Json.obj("status" -> "failure", "message" -> "Unable to connect to mail server"))
+      case Denied | BadSyntax => Ok(Json.obj("status" -> "failure", "message" -> "Server does not allow RCPT inspection"))
+    }
+
   }
 
   def home() = Action {
-    Ok("Hello")
+    Ok(views.html.main.render())
   }
 }
+
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,4 +4,6 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.3.5")
 
+addSbtPlugin("com.eed3si9n" % "sbt-assembly" % "0.13.0")
+
 addSbtPlugin("com.typesafe.sbt" % "sbt-native-packager" % "0.8.0-M1")
